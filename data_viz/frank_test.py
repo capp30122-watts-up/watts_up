d9ae79740553e3df15251c8188e31015dc36ee9d
Frank Vasquez <frankvasquez7@uchicago.edu>
2024-02-19 12:59:21 -0600

    added test data

--- data_viz/frank_test.py
+++ data_viz/frank_test.py
@@ -1,15 +1,12 @@
 # visit http://127.0.0.1:8050/ in your web browser.
 
 import pandas as pd
-
 from dash import Dash, html, dcc
 import plotly.express as px
 import plotly.graph_objects as go
 
 app = Dash(__name__)
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
 df = pd.DataFrame({
     "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
     "Amount": [4, 1, 2, 2, 4, 5],

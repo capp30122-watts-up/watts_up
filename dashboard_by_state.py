9bdd30609abd40a525e2fa5c2f92fab8b8e6d0fa
Wxy-23 <142918746+Wxy-23@users.noreply.github.com>
2024-03-02 12:40:40 -0600

    final version

--- dashboard_by_state.py
+++ dashboard_by_state.py
@@ -3,42 +3,86 @@ import pandas as pd
 from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
 
+#Connect to the database
 engine = create_engine('sqlite:///data_sources/database/plants.db')
 
-query = '''
+# Query for energy generation and carbon emission data
+query_plants = '''
     SELECT year_state, year, state_id,
-    sum(plngenan) as total_energy, sum(plgenatn) as non_renewable_energy, sum(plgenatr) as renewable_energy
+    sum(plngenan) as total_energy, sum(plgenatn) as non_renewable_energy, sum(plgenatr) as renewable_energy,
+    sum(plco2an) as carbon_emission
     FROM plants 
     GROUP BY year_state, year;
 '''
+df_plants = pd.read_sql(query_plants, engine)
 
-df = pd.read_sql(query, engine)
+# Query for energy price data
+query_price = '''
+    SELECT year, stateid, price_all as price_total, price_com as price_commercial, price_ind as price_industrial, price_res as price_residential
+    FROM elec_table
+    GROUP BY year_state, year;
+'''
+df_price = pd.read_sql(query_price, engine)
 
-# Create a Dash app
+
+#Create dashboard
 app = Dash(__name__)
 
 app.layout = html.Div([
-    html.H1("Energy Trends by State"),
+    html.H1("Energy Generation, Emission and Price Trends by State"),
+
+# Dropdown for state selection    
     dcc.Dropdown(
         id='state-dropdown',
-        options=[{'label': state, 'value': state} for state in df['state_id'].unique()],
-        value=df['state_id'].unique()[0]
+        options=[{'label': state, 'value': state} for state in df_plants['state_id'].unique()],
+        value=df_plants['state_id'].unique()[0]
     ),
-    dcc.Graph(id='trend-plot')
+    
+    
+    
+# Dropdown for trend type selection
+    dcc.Dropdown(
+            id='trend-type-dropdown',
+            options=[
+                {'label': 'Energy Generation Trend', 'value': 'energy-trend'},
+                {'label': 'Carbon Emission Trend', 'value': 'carbon-trend'},
+                {'label': 'Energy Price Trend', 'value': 'price-trend'}
+            ],
+            value='energy-trend', 
+            clearable=False
+    ),
+    
+    # Graph for the selected trend
+    dcc.Graph(id='main-plot'),
 ])
 
+
 @app.callback(
-    Output('trend-plot', 'figure'),
-    Input('state-dropdown', 'value')
+    Output('main-plot', 'figure'),
+    [Input('state-dropdown', 'value'),
+     Input('trend-type-dropdown', 'value')]
 )
 
     
-def update_plot(selected_state):
-    filtered_df = df[df['state_id'] == selected_state]
-    long_df = pd.melt(filtered_df, id_vars=['year'], value_vars=['total_energy', 'non_renewable_energy', 'renewable_energy'],
-                      var_name='energy_type', value_name='energy_value')
-    plot = px.line(long_df, x='year', y='energy_value', color='energy_type', title=f"Energy Trends in {selected_state}")
+def update_main_plot(selected_state, selected_trend):
+    if selected_trend == 'energy-trend':    
+        filtered_df = df_plants[df_plants['state_id'] == selected_state]
+        long_df = pd.melt(filtered_df, id_vars=['year'], value_vars=['total_energy', 'non_renewable_energy', 'renewable_energy'],
+                        var_name='energy_type', value_name='energy_value')
+        plot = px.line(long_df, x='year', y='energy_value', color='energy_type', title=f"Energy Generation Trends in {selected_state}")
+
+    elif selected_trend == 'carbon-trend': 
+        filtered_df = df_plants[df_plants['state_id'] == selected_state]
+        plot = px.line(filtered_df, x='year', y='carbon_emission', title=f"Carbon Emission Trends in {selected_state}")   
+        
+    elif selected_trend == 'price-trend':
+        filtered_df = df_price[df_price['stateid'] == selected_state]
+        long_df = pd.melt(filtered_df, id_vars=['year'], value_vars=['price_total', 'price_commercial', 'price_industrial','price_residential'],
+                        var_name='price_type', value_name='price_value')    
+        plot = px.line(long_df, x='year', y='price_value', color='price_type', title=f"Energy Price Trends in {selected_state}")
+     
     return plot
 
+# Run the Dash app
 if __name__ == '__main__':
     app.run_server(debug=True)

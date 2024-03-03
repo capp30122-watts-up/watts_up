c81898e38d4d032af9e8e39948e05f4c18a573f3
jacobtrout <troutjacobm@gmail.com>
2024-03-03 15:52:51 -0600

    cleaned bad files

--- watts_up/run.py
+++ watts_up/run.py
@@ -5,8 +5,7 @@ Run the app or the getdata script, depending on the input provided
 
 # Author: Jacob Trout
 '''
-
-
+import sys
 from watts_up.data_processing import get_data
 from watts_up import app
 
@@ -20,7 +19,18 @@ def run_dash():
     app.app.run_server()
 
 
+message = ('To run the dashboard, type "python -m watts_up dashboard" | '
+            'To run update data, type "python -m watts_up getdata"')
+
+
 def main():
     '''Run the app or the getdata script, depending on the input provided'''
-    run_getdata()
-    run_dash()
+    if len(sys.argv) > 1:
+        if sys.argv[1] == 'dashboard':
+            run_dash()
+        elif sys.argv[1] == 'getdata':
+            run_getdata()
+        else:
+            print("[Invalid Input Provided] " + message )
+    else:
+        print("[No Input Provided] " + message)

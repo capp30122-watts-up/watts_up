00282e59b471b5903941524d649c5318957c1742
jacobtrout <troutjacobm@gmail.com>
2024-03-04 10:40:35 -0600

    praveens shitty map

--- watts_up/data_processing/clean_data/clean_data.py
+++ watts_up/data_processing/clean_data/clean_data.py
@@ -1,8 +1,9 @@
-'''
+"""
 This file is used to clean and format the plant, pricing, gdp, and population 
 data to prepare for database creation
 Authors: Jacob Trout and Praveen Devarajan 
-'''
+"""
+
 import pandas as pd
 import pathlib
 import os
@@ -12,7 +13,8 @@ from io import StringIO
 from watts_up.util.util import COL_NAMES, STATE_MAPPING_DATA
 
 
-DATA_DIR_OUTPUT = (pathlib.Path(__file__).parent.parent.parent / "data/final_data")
+DATA_DIR_OUTPUT = pathlib.Path(__file__).parent.parent.parent / "data/final_data"
+
 
 def clean_plant_data():
     """
@@ -48,7 +50,7 @@ def clean_plant_data():
         df_all = pd.concat([df_all, df2], ignore_index=True)
 
     df_all.columns = df_all.columns.str.lower()
-    
+
     json_data = df_all.to_json(orient="records")
     with open(os.path.join(DATA_DIR_OUTPUT, "cleaned_egrid_data.json"), "w") as f:
         f.write(json_data)
@@ -71,98 +73,133 @@ def clean_price_data():
     """
     pd_list = []
     data_list = []
-    json_file_path = (pathlib.Path(__file__).parent.parent.parent / "data/intermediate_data/api_responses.json")
+    json_file_path = (
+        pathlib.Path(__file__).parent.parent.parent
+        / "data/intermediate_data/api_responses.json"
+    )
     with open(json_file_path, "r") as file:
         responses = json.load(file)
 
     for response in responses:
-        data_list += [response['response']['data']]
+        data_list += [response["response"]["data"]]
 
-    for sublist in  data_list:
+    for sublist in data_list:
         for dict in sublist:
             pd_list += [dict]
     cleaned_df = pd.DataFrame(pd_list)
-    
+
     for index, row in cleaned_df.iterrows():
-        cleaned_df.at[index, 'period'] = pd.to_datetime(row['period'], format='%Y')
-        cleaned_df.at[index, 'year'] = cleaned_df.at[index, 'period'].year
-        cleaned_df.at[index, 'year_state'] = f"{cleaned_df.at[index, 'year']}_{row['stateid']}"
+        cleaned_df.at[index, "period"] = pd.to_datetime(row["period"], format="%Y")
+        cleaned_df.at[index, "year"] = cleaned_df.at[index, "period"].year
+        cleaned_df.at[index, "year_state"] = (
+            f"{cleaned_df.at[index, 'year']}_{row['stateid']}"
+        )
         row.stateid = str(row.stateid)
         row.sectorid = str(row.sectorid)
-    cleaned_df = cleaned_df.drop(columns = ["price-units", "sectorName", "stateDescription","period"])
+    cleaned_df = cleaned_df.drop(
+        columns=["price-units", "sectorName", "stateDescription", "period"]
+    )
 
-    # cleaned_df['stateid'] = cleaned_df['stateid'].astype(str)  
-    # cleaned_df['sectorid'] = cleaned_df['sectorid'].astype(str)  
-    cleaned_df['price'] = pd.to_numeric(cleaned_df['price'], errors='coerce')
+    # cleaned_df['stateid'] = cleaned_df['stateid'].astype(str)
+    # cleaned_df['sectorid'] = cleaned_df['sectorid'].astype(str)
+    cleaned_df["price"] = pd.to_numeric(cleaned_df["price"], errors="coerce")
 
-    df_pivoted = cleaned_df.pivot_table(index=['stateid', 'year', 'year_state'],\
-                                        columns='sectorid', values='price').\
-                                            reset_index()
-    df_pivoted.rename(columns={'ALL': 'price_all', 'COM': 'price_com', 'IND': \
-                            'price_ind', 'RES': 'price_res'}, inplace=True)
+    df_pivoted = cleaned_df.pivot_table(
+        index=["stateid", "year", "year_state"], columns="sectorid", values="price"
+    ).reset_index()
+    df_pivoted.rename(
+        columns={
+            "ALL": "price_all",
+            "COM": "price_com",
+            "IND": "price_ind",
+            "RES": "price_res",
+        },
+        inplace=True,
+    )
     df_pivoted.columns.name = None
 
-    #Writing the cleaned data to a json file
-    json_data = df_pivoted.to_json(orient='records')
+    # Writing the cleaned data to a json file
+    json_data = df_pivoted.to_json(orient="records")
 
     with open(os.path.join(DATA_DIR_OUTPUT, "cleaned_api_responses.json"), "w") as file:
         file.write(json_data)
 
-DATA_DIR = (pathlib.Path(__file__).parent.parent.parent / "data/raw_data/gdp_pop")
+
+DATA_DIR = pathlib.Path(__file__).parent.parent.parent / "data/raw_data/gdp_pop"
 
 STATE_MAPPING = pd.DataFrame(STATE_MAPPING_DATA)
 
+
 def clean_gdp_data():
-    raw_gdp_path = pathlib.Path(DATA_DIR) / 'gdp.csv'
+    raw_gdp_path = pathlib.Path(DATA_DIR) / "gdp.csv"
     raw_gdp = pd.read_csv(raw_gdp_path)
 
-    melted_df = pd.melt(raw_gdp, id_vars=['Years'], var_name='State', value_name='gdp_2022_prices')
-    merged_df = pd.merge(melted_df, STATE_MAPPING, left_on='State', right_on='state', how='left') \
-                .drop(columns=['state', 'State']) \
-                .rename(columns={"Years": 'year'}) 
-    merged_df['year_state'] = merged_df['year'].astype(str) + '_' + merged_df['stateid'].astype(str)
+    melted_df = pd.melt(
+        raw_gdp, id_vars=["Years"], var_name="State", value_name="gdp_2022_prices"
+    )
+    merged_df = (
+        pd.merge(
+            melted_df, STATE_MAPPING, left_on="State", right_on="state", how="left"
+        )
+        .drop(columns=["state", "State"])
+        .rename(columns={"Years": "year"})
+    )
+    merged_df["year_state"] = (
+        merged_df["year"].astype(str) + "_" + merged_df["stateid"].astype(str)
+    )
 
     # Make the json file
-    json_data = merged_df.to_json(orient='records')
-    with open(pathlib.Path(DATA_DIR_OUTPUT) / 'gdp_numbers.json', 'w') as json_file:
+    json_data = merged_df.to_json(orient="records")
+    with open(pathlib.Path(DATA_DIR_OUTPUT) / "gdp_numbers.json", "w") as json_file:
         json_file.write(json_data)
+
 
 def clean_pop_data():
     """
-    Cleans raw population data for the years 2019 and 2022 from CSV files and writes the cleaned data to a JSON file.
-    Reads the raw population data from two CSV files, cleans each separately, merges them,
-    melts to long format, merges with state mapping, drops unnecessary columns, renames columns,
-    creates a new column 'year_state' by combining 'year' and 'stateid', and writes the cleaned DataFrame to a JSON file.
+    Cleans raw population data for the years 2019 and 2022 from CSV files 
+    and writes the cleaned data to a JSON file.
     """
-    pop_2019_path = pathlib.Path(DATA_DIR) / 'p1.csv'
-    pop_2022_path = pathlib.Path(DATA_DIR) / 'p2.csv'
+    pop_2019_path = pathlib.Path(DATA_DIR) / "p1.csv"
+    pop_2022_path = pathlib.Path(DATA_DIR) / "p2.csv"
     pop_2019 = pd.read_csv(pop_2019_path)
     pop_2022 = pd.read_csv(pop_2022_path)
 
     # Cleaning pop_2019
-    pop_2019 = pop_2019.drop(columns=['4/1/2010 Census population!!Population',\
-                                       '4/1/2010 population estimates base!!Population'])
+    pop_2019 = pop_2019.drop(
+        columns=[
+            "4/1/2010 Census population!!Population",
+            "4/1/2010 population estimates base!!Population",
+        ]
+    )
     col_names = pop_2019.columns.tolist()
     year_cols = [col_names[0]]
     for column in col_names[1:]:
-        year_cols.append(re.findall(r'(\d{4})', str(column))[0])
+        year_cols.append(re.findall(r"(\d{4})", str(column))[0])
     pop_2019.columns = year_cols
-    pop_2019 = pop_2019.rename(columns={"Geographic Area Name (Grouping)": 'state'})
+    pop_2019 = pop_2019.rename(columns={"Geographic Area Name (Grouping)": "state"})
 
     # Cleaning pop_2022
-    pop_2022['State'] = pop_2022['State'].str.lstrip('.')
-    pop_2022 = pop_2022.rename(columns={"State": 'state'})
+    pop_2022["State"] = pop_2022["State"].str.lstrip(".")
+    pop_2022 = pop_2022.rename(columns={"State": "state"})
 
-    merged_df = pd.merge(pop_2019, pop_2022, left_on='state', right_on='state', how='left')
-    merged_df = merged_df[merged_df['state'] != 'Puerto Rico']
-    pop_final = pd.melt(merged_df, id_vars=['state'], var_name='year', value_name='population')
-    merged_df1 = pd.merge(pop_final, STATE_MAPPING, left_on='state', right_on='state', how='left')
-    merged_df1['year_state'] = merged_df1['year'].astype(str) + '_' + merged_df1['stateid'].astype(str)
-    merged_df1 = merged_df1.drop(columns=['state'])
-    merged_df1['year'] = merged_df1['year'].astype(int)
-    merged_df1['population'] = merged_df1['population'].str.replace(',', '').astype(int)
-    
-    #Make the json file
-    json_data = merged_df1.to_json(orient='records')
-    with open(os.path.join(DATA_DIR_OUTPUT,"pop_numbers.json"), 'w') as json_file:
+    merged_df = pd.merge(
+        pop_2019, pop_2022, left_on="state", right_on="state", how="left"
+    )
+    merged_df = merged_df[merged_df["state"] != "Puerto Rico"]
+    pop_final = pd.melt(
+        merged_df, id_vars=["state"], var_name="year", value_name="population"
+    )
+    merged_df1 = pd.merge(
+        pop_final, STATE_MAPPING, left_on="state", right_on="state", how="left"
+    )
+    merged_df1["year_state"] = (
+        merged_df1["year"].astype(str) + "_" + merged_df1["stateid"].astype(str)
+    )
+    merged_df1 = merged_df1.drop(columns=["state"])
+    merged_df1["year"] = merged_df1["year"].astype(int)
+    merged_df1["population"] = merged_df1["population"].str.replace(",", "").astype(int)
+
+    # Make the json file
+    json_data = merged_df1.to_json(orient="records")
+    with open(os.path.join(DATA_DIR_OUTPUT, "pop_numbers.json"), "w") as json_file:
         json_file.write(json_data)
--- watts_up/pages/predict.py
+++ watts_up/pages/predict.py
@@ -1,28 +1,16 @@
-'''
-    Creates an animated Dash component visualizing the predicted year by which 
-    each state is expected to reach 60% renewable energy.
-
-    Author: Frank Vasquez
-'''
 import dash
 import pandas as pd
 import plotly.express as px
 from dash import html, dcc
 
+# Function to create a Dash component for renewable energy predictions
 dash.register_page(__name__)
 
-def create_animated_renewable_energy_dash_component():
-    '''
-    This function loads a dataset from a CSV file containing states and their 
-    respective predicted years for achieving 60% renewable energy.
+def create_renewable_energy_dash_component():
+    data = pd.read_csv('watts_up/data/final_data/place_holder_predictions.csv')
 
-     Returns:
-        A Dash HTML containing the animated bar chart
-    '''
-    data = pd.read_csv('watts_up/data_viz/place_holder_predictions.csv')
-
-    data['predicted_year'] = pd.to_numeric(data['predicted_year'], errors='coerce', downcast='integer')
-
+    predictable_data = data[data['predicted_year'] != 'Not predictable'].copy()
+    predictable_data['predicted_year'] = pd.to_numeric(predictable_data['predicted_year'], errors='coerce')
 
     not_predictable_states = data[data['predicted_year'] == 'Not predictable']['state_id'].tolist()
     not_predictable_notice = "Note: Some states have already reached the level \
@@ -42,17 +30,15 @@ def create_animated_renewable_energy_dash_component():
                       xaxis_title="State",
                       yaxis_title="Predicted Year",
                       plot_bgcolor="white",
-                      transition={'duration': 50}, 
                       xaxis=dict(showline=True, showgrid=False, linecolor='black'),
                       yaxis=dict(showgrid=True, gridcolor='lightgrey'),
                       )
-
     fig.update_xaxes(tickangle=45)
 
     # Set the range of the color scale to cover the predicted years
     fig.update_coloraxes(colorbar=dict(title='Predicted Year'))
 
-
+    # Return a Dash layout component
     return html.Div([
         html.H1("Renewable Energy Predictions Map"),
         dcc.Graph(id='choropleth-map', figure=fig),
